68d9a7297c84ccf1ff377cbb646d3d03d615c652
Marcus Libäck <marcus.liback@gmail.com>
2020-06-15 11:31:18 +0200

    Add encoding handling: -Encoding option or automatic detection if not specified

--- Split-File.psm1
+++ Split-File.psm1
@@ -5,7 +5,6 @@
     
 .EXAMPLE
     PS C:\> Split-File foo.txt -Split 100 -Header 2 -AddHeaders
-    Explanation of what the example does
 .INPUTS
     Inputs (if any)
 .OUTPUTS
@@ -13,64 +12,125 @@
 .NOTES
     General notes
 #>
+
+function Get-Encoding {
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [string]
+        $Path
+    )
+
+    process {
+        $bom = New-Object -TypeName System.Byte[](4)
+        $file = New-Object System.IO.FileStream($Path, 'Open', 'Read')
+
+        $null = $file.Read($bom,0,4)
+        $file.Close()
+        $file.Dispose()
+
+        $enc = "Default"
+        if ($bom[0] -eq 0x2b -and $bom[1] -eq 0x2f -and $bom[2] -eq 0x76)
+            { $enc = "UTF7" }
+        if ($bom[0] -eq 0xff -and $bom[1] -eq 0xfe)
+            { $enc = "Unicode" }
+        if ($bom[0] -eq 0xfe -and $bom[1] -eq 0xff)
+            { $enc = "BigEndianUnicode" }
+        if ($bom[0] -eq 0x00 -and $bom[1] -eq 0x00 -and $bom[2] -eq 0xfe -and $bom[3] -eq 0xff)
+            { $enc = "UTF32" }
+        if ($bom[0] -eq 0xef -and $bom[1] -eq 0xbb -and $bom[2] -eq 0xbf)
+            { $enc = "UTF8" }
+        
+        $enc
+    }
+}
+
 function Split-File {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true,
-                   Position=0,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage="Path to one or more files.")]
+                    Position=0,
+                    ValueFromPipeline=$true,
+                    ValueFromPipelineByPropertyName = $true,
+                    HelpMessage="Path to one or more files.")]
         [ValidateNotNullOrEmpty()]
         [SupportsWildcards()]
-        [string[]]
-        $Path,
+        [string[]] $Path,
+
+        [Parameter(Mandatory=$false,
+                    Position=1,
+                    HelpMessage="Export path of converted file")]
+        [string[]] $ExportPath,
+        
         [Parameter(Mandatory=$true,
-                   HelpMessage="Number of lines to split input file on")]
+                    HelpMessage="Number of lines to split input file on")]
         [ValidateRange(1, [int]::MaxValue)]
-        [int]
-        $Split,
+        [int] $Split,
+        
         [Parameter(Mandatory=$false,
-                   HelpMessage="Number of header rows in input file (defaults to 1)")]
+                    HelpMessage="Number of header rows in input file (defaults to 1)")]
         [ValidateRange(0, [int]::MaxValue)]
-        [int]
-        $Header = 1,
+        [int] $Header = 1,
+        
         [Parameter(Mandatory=$false,
-                   HelpMessage="Add header from original file to resulting files (default to false")]
-        [switch]
-        $AddHeaders
+                    HelpMessage="Skip header from original file in resulting files (default to false")]
+        [switch] $SkipHeader,
+
+        [Parameter(Mandatory=$false,
+                    HelpMessage="Encoding to use on both input and output files,tries to guess encoding if not specified")]
+        [ValidateSet('Default','ASCII','UTF7','UTF8', 'Unicode','UTF32', 'BigEndianUnicode')]
+        [string] $Encoding
     )
-    
+
     begin {
         if (Test-Path -PathType Leaf -Path $Path) {
             $files = Get-ChildItem -File $Path
         } else {
             Write-Error "Invalid path"
-            exit 1
+            break
+        }
+
+
+        # Set up the export path
+        if (!$ExportPath -or !(Test-Path $ExportPath -PathType Container)) {
+            $ExportPath = Split-Path $Path -Resolve
         }
     }
     
     process {
         $Processed = 0
         foreach ($file in $files) {
+            $FilePath = ((Resolve-Path $file.FullName).ToString() -replace "(^.*::)")
             $ProgressActivity = "Splitting $($file.Name):"
             Write-Host "Splitting $($file.Name)"
             Write-Progress -Activity $ProgressActivity `
-                -Status "Calculaing total number of output files" `
+                -Status "Calculating total number of output files" `
                 -PercentComplete 0
-            
-            try {
-                $FileReader = New-Object System.IO.StreamReader $file
-            } catch {
-                Write-Error "Error: Could not open $($file.FullName)"
-                break
+                
+            if (-not $Encoding) {
+                $Encoding = Get-Encoding -Path ((Resolve-Path $Path).ToString() -replace "(^.*::)")
+            }
+            $EncodingObject = switch ($Encoding) {
+                "UTF7"              { [System.Text.Encoding]::UTF7 ; break }
+                "Unicode"           { [System.Text.Encoding]::Unicode ; break }
+                "BigEndianUnicode"  { [System.Text.Encoding]::BigEndianUnicode ; break }
+                "UTF32"             { [System.Text.Encoding]::UTF32 ; break }
+                "UTF8"              { [System.Text.Encoding]::UTF8 ; break }
+                default             { [System.Text.Encoding]::Default ; break }
             }
             
-            $TotalLines     = -$Header + $((Get-Content $file -ReadCount 1000 | ForEach-Object {$x += $_.Count });$x)
+            try {
+                $FileReader = New-Object System.IO.StreamReader $FilePath, $EncodingObject
+            } catch {
+                Write-Error "Error: Could not open $($FilePath)"
+                break
+            }
+
+            $TotalLines     = -$Header + $((Get-Content $file -ReadCount 1000 -Encoding "Default" | ForEach-Object {$x += $_.Count });$x)
             $TotalBatches   = [Math]::Ceiling($TotalLines / $Split)
             $FileHeader     = [System.Collections.ArrayList]@()
             
-            $Counter        = 0
+            $Counter = 0
             while ($Counter -lt $Header) {
                 if (($Counter % ($Header / 20)) -eq 0 -or $Counter -eq 0) {
                     Write-Progress -Activity $ProgressActivity `
@@ -84,25 +144,25 @@ function Split-File {
             
             $Batch = 1
             while ($FileReader.EndOfStream -ne $true) {
-                $ExportDir = $file | Split-Path -Parent
-                $ExportFile = $file.BaseName + "_" `
-                + $Batch.ToString().PadLeft($TotalBatches.ToString().Length, '0') `
-                + $file.Extension
-                
+                $ExportFile = (Join-Path -Path $ExportPath `
+                    -ChildPath ($file.BaseName + "_" `
+                    + $Batch.ToString().PadLeft($TotalBatches.ToString().Length, '0') `
+                    + $file.Extension)).ToString() -replace "(^.*::)"
+
                 Write-Host "-> $ExportFile"
                 Write-Progress -Activity $ProgressActivity `
                     -Status "Writing $ExportFile (of $TotalBatches)" `
                     -PercentComplete ([Math]::Ceiling($Batch / $TotalBatches * 100))
                 
                 try {
-                    $FileWriter = New-Object System.IO.StreamWriter `
-                        -ArgumentList $(Join-Path -Path $ExportDir -ChildPath $ExportFile)
+                    $FileWriter = New-Object System.IO.StreamWriter $ExportFile, $false, $EncodingObject
                 } catch {
+                    $_.Exception.Message
                     Write-Error "Error: Could not write file: $ExportFile"
                     break
                 }
                 
-                if ($AddHeaders) {
+                if (!$SkipHeader) {
                     foreach ($line in $FileHeader) {
                         $FileWriter.WriteLine($line)
                     }
@@ -125,9 +185,11 @@ function Split-File {
     }
     
     end {
-        $FileReader.Close()
-        $FileWriter.Close()
+        $FileReader.Close() | Out-Null
+        $FileWriter.Close() | Out-Null
 
         Write-Host "`n$Processed file(s) processed`n"
     }
 }
+
+Export-ModuleMember -Function Split-File
